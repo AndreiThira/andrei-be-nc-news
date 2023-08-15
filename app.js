8cbe7fc55469bb17d62ed2a62ffe0ca569bdc341
AndreiThira <andreithira03@gmail.com>
2023-08-15 12:05:54 +0100

    This commit will complete task 3 and the respective tests

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
-const {getAllTopics, getAllEndpoints} = require("./controllers/topics-controller")
+const {getAllTopics, getAllEndpoints, getArticleByID} = require("./controllers/topics-controller")
 
 
 app.get("/api/topics", getAllTopics)
 app.get("/api", getAllEndpoints)
+app.get("/api/articles/:article_id", getArticleByID)
 
+app.use((err, req, res, next) => {
+    if (err.status) {
+      res.status(err.status).send({ message: err.message });
+    } else next(err);
+  });
+
+app.use((err,request, response, next)=>{
+    if (err.code === "22P02"){
+        response.status(400).send({message: "Invalid Article ID"})
+    } else {
+        next(err)
+    }
+})
 
 app.use((err, request, response, next)=>{
     response.status(500).send({message: "Internal Server Error"})
--- models/topic-model.js
+++ models/topic-model.js
@@ -8,4 +8,18 @@ const fetchAllTopics = () => {
   });
 };
 
-module.exports = { fetchAllTopics,};
+const fetchArticleByID = (articleID)=>{
+  return db.query("SELECT * FROM articles WHERE article_id = $1;", [articleID]).then(({rows})=>{
+    const article = rows[0]
+    if(!article){
+      return Promise.reject({
+        status:404,
+        message: "No article found"
+      })
+    }
+    return rows;
+  })
+}
+
+
+module.exports = { fetchAllTopics, fetchArticleByID};
