ebb975d037ca7800088ca3b365b31c05d4c1c062
AndreiThira <andreithira03@gmail.com>
2023-08-17 08:42:00 +0100

    This commmit will refactor the models to be using .then instead of async/await

--- models/topic-model.js
+++ models/topic-model.js
@@ -21,38 +21,50 @@ const fetchArticleByID = (articleID)=>{
   })
 }
 
-const fetchAllArticles = () =>{
+const fetchAllArticles = () => {
   return db.query(`SELECT 
-  articles.article_id,
-  articles.author,
-  articles.title,
-  topic,
-  articles.created_at,
-  articles.votes,
-  article_img_url,
-  COUNT(comments.article_id) AS comment_count
-FROM
-  articles
-LEFT JOIN
-  comments ON articles.article_id = comments.article_id
-GROUP BY
-  articles.article_id
-ORDER BY
-  created_at DESC;`).then(({rows})=>{
-    return rows
-  })
-}
-
-const fetchCommentsByArticle = async (article_id) => {
-  const articleExists = await db.query("SELECT COUNT(*) FROM articles WHERE article_id = $1;", [article_id]);
-
-  if (articleExists.rows[0].count === '0') {
-    return Promise.reject({status:404, message: "Article not found"})
-  }
-
-  const comments = await db.query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;", [article_id]);
-  return comments.rows; 
+      articles.article_id,
+      articles.author,
+      articles.title,
+      topic,
+      articles.created_at,
+      articles.votes,
+      article_img_url,
+      COUNT(comments.article_id) AS comment_count
+    FROM
+      articles
+    LEFT JOIN
+      comments ON articles.article_id = comments.article_id
+    GROUP BY
+      articles.article_id
+    ORDER BY
+      created_at DESC;`)
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => {
+      next(err);
+    });
 };
+
+
+const fetchCommentsByArticle = (article_id) => {
+  return db.query("SELECT COUNT(*) FROM articles WHERE article_id = $1;", [article_id])
+    .then(articleExistsResult => {
+      if (articleExistsResult.rows[0].count === '0') {
+        return Promise.reject({ status: 404, message: "Article not found" })
+      }
+
+      return db.query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;", [article_id])
+    })
+    .then(commentsResult => {
+      return commentsResult.rows
+    })
+    .catch(err => {
+      throw err
+    });
+};
+
 
 
 module.exports = { fetchAllTopics, fetchArticleByID, fetchAllArticles, fetchCommentsByArticle};
